12482ee7b865f498ddf3746ac1cc598b21d9966f
dnaccarato22 <49168358+dnaccarato22@users.noreply.github.com>
2020-06-18 15:31:17 -0400

    Finished import/export functionality (not pretty yet)

--- IPAddress/src/IPAddressDriver.java
+++ IPAddress/src/IPAddressDriver.java
@@ -1,13 +1,13 @@
 
 /**
- * driver for ip address program
+ * driver for IP address program
  * @author cchiu 
- *
+ * 
  */
 public class IPAddressDriver {
 	
 	public static void main(String[] args) {
-		new GUIFrame();	
+		new GUIFrame("list.csv");	
 	}
 	
 }
